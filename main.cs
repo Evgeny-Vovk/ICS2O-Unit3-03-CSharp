5db28028f8a12d61831d04a576e349315361cc4c
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-04-14 17:03:35 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+// Created by: Evgeny
+// Created on: Mar 2022
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //input
+        float radius;
+
+        Console.WriteLine("This program finds the volume of a sphere.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the radius(cm): ");
+        radius = Convert.ToInt32(Console.ReadLine());
+
+        //process
+        double volume = 4.0 / 3.0 * Math.PI * Math.Pow(radius, 3);
+      
+
+        //output
+        Console.WriteLine("");
+        Console.WriteLine("The volume is: " + volume.ToString("0.00") + " cm³.");
+
+        Console.WriteLine("\nDone.");
+    }
+}
